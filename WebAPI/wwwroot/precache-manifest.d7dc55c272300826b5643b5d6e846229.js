21eacecdbb7eb75c2a697fd8f3060ed9896e37af
AlexDesign <luisalex7777777@gmail.com>
2021-09-17 11:35:07 -0500

    insertando cambios de prubas

---
+++ WebAPI/wwwroot/precache-manifest.d7dc55c272300826b5643b5d6e846229.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652bc83dedbbbda0e2d12a5e6ea733ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66dcd04ea75fb923a1cd",
+    "url": "/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "0e30166f5b0d90291f83",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "66dcd04ea75fb923a1cd",
+    "url": "/static/js/2.9e5fc193.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9e5fc193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e30166f5b0d90291f83",
+    "url": "/static/js/main.98428ed0.chunk.js"
+  },
+  {
+    "revision": "9ddbcd4877b5ea821941",
+    "url": "/static/js/runtime-main.4e428d27.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
